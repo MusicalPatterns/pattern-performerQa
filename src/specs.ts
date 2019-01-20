dc1121cbdc243be25dc7861e0ce60235ada108da
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-20 10:48:07 -0800

    1.0.49: onekindaspec

--- src/specs.ts
+++ src/specs.ts
@@ -1,20 +1,20 @@
 import {
-    standardInitialPatternSpec,
-    StandardPatternSpec,
-    standardPatternSpecAttributes,
-    StandardPatternSpecData,
-    StandardPatternSpecProperties,
+    standardInitialSpec,
+    StandardSpec,
+    standardSpecAttributes,
+    StandardSpecData,
+    StandardSpecProperties,
 } from '@musical-patterns/pattern'
 import { PERFORMER_QA_PATTERN_DURATION_SCALAR, PERFORMER_QA_PATTERN_PITCH_SCALAR } from './constants'
 
-const initial: StandardPatternSpec = {
-    ...standardInitialPatternSpec,
-    [ StandardPatternSpecProperties.PATTERN_DURATION_SCALAR ]: PERFORMER_QA_PATTERN_DURATION_SCALAR,
-    [ StandardPatternSpecProperties.PATTERN_PITCH_SCALAR ]: PERFORMER_QA_PATTERN_PITCH_SCALAR,
+const initial: StandardSpec = {
+    ...standardInitialSpec,
+    [ StandardSpecProperties.PATTERN_DURATION_SCALAR ]: PERFORMER_QA_PATTERN_DURATION_SCALAR,
+    [ StandardSpecProperties.PATTERN_PITCH_SCALAR ]: PERFORMER_QA_PATTERN_PITCH_SCALAR,
 }
 
-const specData: StandardPatternSpecData = {
-    attributes: standardPatternSpecAttributes,
+const specData: StandardSpecData = {
+    attributes: standardSpecAttributes,
     initial,
 }
 
