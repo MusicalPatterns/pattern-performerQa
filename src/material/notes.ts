a141292fa37a4c7d002b73c0c55e537ef6e05a28
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-29 12:54:24 -0800

    1.0.71: standards not defaults

--- src/material/notes.ts
+++ src/material/notes.ts
@@ -1,5 +1,5 @@
 import { NoteSpec } from '@musical-patterns/compiler'
-import { DEFAULT_DURATIONS_SCALE_INDEX, DEFAULT_PITCH_SCALE_INDEX } from '@musical-patterns/pattern'
+import { STANDARD_DURATIONS_SCALE_INDEX, STANDARD_PITCH_SCALE_INDEX } from '@musical-patterns/pattern'
 import { apply, to } from '@musical-patterns/utilities'
 import { CENTER_BLOCKS_ON_ORIGIN } from './constants'
 
@@ -8,11 +8,11 @@ const buildNoteSpec: (blockElement: number) => NoteSpec =
         ({
             durationSpec: {
                 scalar: to.Scalar(blockElement),
-                scaleIndex: DEFAULT_DURATIONS_SCALE_INDEX,
+                scaleIndex: STANDARD_DURATIONS_SCALE_INDEX,
             },
             pitchSpec: {
                 index: to.Index(blockElement),
-                scaleIndex: DEFAULT_PITCH_SCALE_INDEX,
+                scaleIndex: STANDARD_PITCH_SCALE_INDEX,
             },
             positionSpec: [
                 {
