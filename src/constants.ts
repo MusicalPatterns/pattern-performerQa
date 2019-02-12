aa2b7d0e39ecaf0c2bad1e3898b3c1d9e2b60c91
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-11 20:34:58 -0800

    1.0.80: take advantage of non-conflicting units and operation

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 // tslint:disable no-magic-numbers
 
-import { Hz, Ms, to } from '@musical-patterns/utilities'
+import { Hz, Ms, Scalar, to } from '@musical-patterns/utilities'
 
-const PERFORMER_QA_INITIAL_BASE_DURATION: Ms = to.Ms(100)
-const PERFORMER_QA_INITIAL_BASE_FREQUENCY: Hz = to.Hz(100)
+const PERFORMER_QA_INITIAL_BASE_DURATION: Scalar<Ms> = to.Scalar(to.Ms(100))
+const PERFORMER_QA_INITIAL_BASE_FREQUENCY: Scalar<Hz> = to.Scalar(to.Hz(100))
 
 export {
     PERFORMER_QA_INITIAL_BASE_DURATION,
