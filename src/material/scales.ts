e1d4a03065b5f08717b7b84b1bca0d96184e1182
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-25 10:16:09 -0800

    1.0.84: use standard scales

--- src/material/scales.ts
+++ src/material/scales.ts
@@ -1,36 +1,15 @@
 import { BuildScalesFunction, Scale } from '@musical-patterns/compiler'
-import { buildStandardScales, StandardSpec, StandardSpecProperties } from '@musical-patterns/pattern'
-import { from, NO_TRANSLATION, Scalar, to, Translation } from '@musical-patterns/utilities'
+import { buildFlatDurationsScale, buildStandardScales, StandardSpec } from '@musical-patterns/pattern'
+import { Scalar } from '@musical-patterns/utilities'
 
 const buildScales: BuildScalesFunction =
     (spec: StandardSpec): Scale[] => {
-        const { nonScale, flatDurationsScale } = buildStandardScales()
+        const flatDurationsScalars: Scalar[] = buildFlatDurationsScale().scalars || []
 
-        const gainScale: Scale = nonScale
-        const durationScalar: Scalar =
-            from.Ms(spec[ StandardSpecProperties.BASE_DURATION ] || to.Scalar(to.Ms(1)))
-        const durationTranslation: Translation =
-            from.Ms(spec[ StandardSpecProperties.DURATION_TRANSLATION ] || to.Ms(NO_TRANSLATION))
-        const durationsScale: Scale = {
-            scalar: durationScalar,
-            scalars: flatDurationsScale.scalars,
-            translation: durationTranslation,
-        }
-        const pitchesScalar: Scalar =
-            from.Hz(spec[ StandardSpecProperties.BASE_FREQUENCY ] || to.Scalar(to.Hz(1)))
-        const pitchesTranslation: Translation =
-            from.Hz(spec[ StandardSpecProperties.FREQUENCY_TRANSLATION ] || to.Hz(NO_TRANSLATION))
-        const pitchesScale: Scale = {
-            scalar: pitchesScalar,
-            scalars: flatDurationsScale.scalars,
-            translation: pitchesTranslation,
-        }
-
-        return [
-            gainScale,
-            durationsScale,
-            pitchesScale,
-        ]
+        return buildStandardScales(
+            spec,
+            { durationScalars: flatDurationsScalars, pitchScalars: flatDurationsScalars },
+        )
     }
 
 export {
