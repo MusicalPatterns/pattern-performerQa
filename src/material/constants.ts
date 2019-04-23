87e3138aee74f979f01e8ba36089f57d429ed139
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-04-23 16:36:47 -0700

    1.0.142: Point and Translation for Ms

--- src/material/constants.ts
+++ src/material/constants.ts
@@ -1,10 +1,10 @@
 // tslint:disable no-magic-numbers
 
-import { as, Cardinal, Ms, negative } from '@musical-patterns/utilities'
+import { as, Cardinal, Ms, negative, Translation } from '@musical-patterns/utilities'
 
 const CENTER_BLOCKS_ON_ORIGIN: Cardinal = as.Cardinal(negative(4))
 
-const MATERIAL_QA_DELAY: Ms = as.Ms(1000)
+const MATERIAL_QA_DELAY: Translation<Ms> = as.Translation<Ms>(1000)
 
 export {
     CENTER_BLOCKS_ON_ORIGIN,
