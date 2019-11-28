74d33e0d36576f6b77cb60ab883ebeafd6eaadb1
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-11-27 17:15:46 -0800

    1.0.164: ts3.7

--- src/patterns.ts
+++ src/patterns.ts
@@ -1,17 +1,17 @@
 import { Id } from '@musical-patterns/id'
-import { Patterns, StandardPattern } from '@musical-patterns/pattern'
+import { Pattern, Patterns } from '@musical-patterns/pattern'
 import { delayMaterial, material } from './material'
 import { delayMetadata, metadata } from './metadata'
 import { spec } from './spec'
 
-const pattern: StandardPattern = {
+const pattern: Pattern = {
     id: Id.MATERIAL_QA,
     material,
     metadata,
     spec,
 }
 
-const delayPattern: StandardPattern = {
+const delayPattern: Pattern = {
     id: Id.MATERIAL_QA_DELAY,
     material: delayMaterial,
     metadata: delayMetadata,
