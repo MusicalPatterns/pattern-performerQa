5e1ac18e5c9a7dfbf2c96fe84d242dff6c77c01a
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-02-23 23:49:44 -0800

    1.0.167: thunks, required spces, return types, code formatting, and a big change to xenharmonic series

--- src/spec/specs.ts
+++ src/spec/specs.ts
@@ -1,10 +1,4 @@
-import {
-    Spec,
-    Specs,
-    standardConfigurations,
-    standardInitialSpecs,
-    StandardSpec,
-} from '@musical-patterns/spec'
+import { Spec, Specs, standardConfigurations, standardInitialSpecs, StandardSpec } from '@musical-patterns/spec'
 import { MATERIAL_QA_INITIAL_HZ_PHYSICALIZATION, MATERIAL_QA_INITIAL_MS_PHYSICALIZATION } from './constants'
 
 const initialSpecs: Specs = {
